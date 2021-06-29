b4d4ca0ec7f8daf09fea97e0e03ba3e8999df88d
sligo <13173952388@163.com>
2021-06-29 20:48:54 +0800

    未完整

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-let sort = (numbers) => {
-    if (numbers.length > 2) {
+let sort = (numbers) =>{
+    for(let i=0;i<numbers.length-1;i++){
         let index = minIndex(numbers)
-        let min = numbers[index]
-        numbers.splice(index, 1)
-        return [min].concat(sort(numbers))
-    } else {
-        return numbers[0] < numbers[1] ? numbers : numbers.reverse()
+        swap(numbers,index,i)
     }
 }
-let minIndex = (numbers) => {
+let minIndex = (numbers) =>{
     let index = 0
-    for (let i = 1; i < numbers.length; i++) {
-        if (numbers[i] < numbers[index]) {
-            return index = i
+    for(let i=1;i<numbers.length;i++){
+        if(numbers[i]<numbers[index]){
+            index=i
         }
     }
     return index
 }
-let min = (numbers) => {
-    if (numbers.length > 2) {
-        return min([numbers[0], min(numbers.slice(1))])
-    } else {
-        Math.min.apply(null, numbers)
-    }
+let swap =(array,i,j)=>{
+    let temp=array[i]
+    array[i]=array[j]
+    array[j]=temp
 }
