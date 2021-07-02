9add7dfbcffc1eee28b35b8f04b31db433f9b2be
sligo <13173952388@163.com>
2021-07-02 14:44:35 +0800

    计数排序

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-let mergeSort = arr => {
-    let k = arr.length
-    if (k === 1) { return arr }
-    let left = arr.slice(0, Math.floor(k / 2))
-    let right = arr.slice(Math.floor(k / 2))
-    return merge(mergeSort(left), mergeSort(right))
-}
-let merge = (a, b) => {
-    if (a.length === 0) { return b }
-    if (b.length === 0) { return a }
-    return a[0] > b[0] ? [b[0]].concat(merge(a, b.slice(1))) : [a[0]].concat(merge(a.slice(1), b))
+let countSort = arr => {
+    let hashTable = {},
+        max = 0,
+        result = []
+    for (let i = 0; i < arr.length; i++) {
+        if (!(arr[i] in hashTable)) {
+            hashTable[arr[i]] = 1
+        } else {
+            hashTable[arr[i]] += 1
+        }
+        if (max < arr[i]) { max = arr[i] }
+    }
+    for (let j = 0; j <= max; j++) {
+        if (j in hashTable) {
+            for (let h = 0; h < hashTable[j]; h++) { //当j有2个及以上时
+                result.push(j)
+            }
+        }
+    }
+    return result
 }
